ab0da95af4095b72c7638e48aef0886e90d1cff4
atsushi.koga <stbe51@gmail.com>
2020-01-13 22:07:30 +0900

    create php project

---
+++ public/src/Person.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\src;
+
+class Person
+{
+    public function name(): string
+    {
+        return 'Bob';
+    }
+}
---
+++ public/test/PersonTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\test;
+
+use App\src\Person;
+use PHPUnit\Framework\TestCase;
+
+class PersonTest extends TestCase
+{
+    public function testName()
+    {
+        $this->assertEquals(true, true);
+    }
+}
